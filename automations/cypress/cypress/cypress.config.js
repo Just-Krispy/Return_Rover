b6e00e684aab575360442dbd07e5ff3af0ec7ad4
Krispy <149973066+Just-Krispy@users.noreply.github.com>
2023-12-08 19:03:24 -0500

    Update cypress.config.js
    approved

--- automations/cypress/cypress/cypress.config.js
+++ automations/cypress/cypress/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    experimentalStudio: true
+    experimentalStudio: true,
     },
   },
 });
