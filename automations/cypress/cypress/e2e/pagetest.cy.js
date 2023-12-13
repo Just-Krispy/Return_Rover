dd9fa4392f215f5a044c33da75ee38456a6afadb
Krispy <149973066+Just-Krispy@users.noreply.github.com>
2023-12-13 16:43:48 -0500

    automation update
    approved

--- automations/cypress/cypress/e2e/pagetest.cy.js
+++
@@ -1,7 +0,0 @@
-// cypress/integration/navigation.spec.js
-describe('Navigation Test', () => {
-  it('Should navigate to the Return Rover page', () => {
-    cy.visit('https://just-krispy.github.io/Return_Rover/');
-    cy.url().should('include', 'Return_Rover');
-  });
-});
